08ed1ec2d74b3e97d2852715340e3ab6e1945d9f
Damien <ultrarushgame@gmail.com>
2024-10-07 15:09:44 +0200

    [ADD] test new module16

---
+++ module16/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2024 Moka - Horvat Damien
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': '',
+    'summary': '',
+    'version': '16.0.0.0.1',
+    'category': '',
+    "author": "Moka",
+    "website": "https://www.mokatourisme.fr",
+    'depends': [''],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'AGPL-3',
+}
